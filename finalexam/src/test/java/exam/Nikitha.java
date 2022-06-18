ab1363b87c9b97044a135d53dd13371c94900d27
ADMIN <ADMIN@Nikitha>
2022-06-18 12:11:11 +0530

    Final exam

---
+++ finalexam/src/test/java/exam/Nikitha.java
@@ -0,0 +1,33 @@
+package exam;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+
+
+public class Nikitha {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("http://www.variflight.com/en/flight-status");
+		driver.findElement(By.xpath("//input[@id='search-flight-num']")).sendKeys("SQ5905");
+		driver.findElement(By.xpath("//div[@id='searchBtn']")).click();
+		String Actualtitle = driver.getTitle();
+		String expectedTitle = "VariFlight—Living on time! We are a leading global flight data services company. We provide flight data such as historical flight status and schedule, realtime flight information and intergarated data such as flight happiness index";
+		System.out.print("Site title "+Actualtitle);
+		Assert.assertEquals(expectedTitle, Actualtitle);
+		System.out.print("Assertion passed");
+//		Thread.sleep(3000);
+		
+		driver.quit();
+
+	}
+
+}
